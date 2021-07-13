74cc4fc28855f7c2e52cccf18a27f3a9341fa1be
Matheus Gomes <matheusgmaciel@gmail.com>
2021-07-13 17:22:23 -0300

    Try to edit "Cart" routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './checkout/cart/cart.component';
+
 
 const routes: Routes = [
   {
-    path: '',
-    component: ProductsComponent,
+    path: 'Cart',
+    component: CartComponent,
   },
 ];
 
